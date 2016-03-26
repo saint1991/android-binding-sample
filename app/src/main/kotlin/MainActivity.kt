b11cec7409709b3617dc9bd3a172dc4679c3a153
Mizuno <se.mizuno.1122@gmail.com>
2016-03-27 02:42:25 +0900

    Initial Commit

---
+++ app/src/main/kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import saint.co.jp.bindingsample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
